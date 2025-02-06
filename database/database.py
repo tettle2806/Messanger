0ad583d2604e42efb1f5ec55961bae0dd2a051b4
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-02-06 13:02:32 +0500

    database configs

---
+++ database/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import func
+from datetime import datetime
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine, AsyncSession
+
+database_url = "postgresql+asyncpg://postgres:postgres@localhost:5432/messanger"
+engine = create_async_engine(url=database_url, echo=True)
+async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
+
+
+class Base(DeclarativeBase):
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
